f09fa9fc097a2a428c43245ddd542ac1370c1adc
James Elliott <james@deepsymmetry.org>
2018-12-11 21:15:13 -0600

    Improve long-offset artist name handling.
    Now has a much clearere structure in the .ksy *and* provides a single,
    unified API for the struct user to access the name however it was
    stored.

--- src/main/java/org/deepsymmetry/cratedigger/Database.java
+++ src/main/java/org/deepsymmetry/cratedigger/Database.java
@@ -304,13 +304,8 @@ public class Database implements Closeable {
                 final long id = artistRow.id();
                 index.put(id, artistRow);
 
-                // Index the artist ID by name as well, handling the special case of extra long names.
-                String name;
-                if (artistRow.longName() != null) {
-                   name = getText(artistRow.longName());
-                } else {
-                    name = getText(artistRow.name());
-                }
+                // Index the artist ID by name as well.
+                final String  name = getText(artistRow.name());
                 if (name.length() > 0) {
                     addToSecondaryIndex(nameIndex, name, id);
                 }
@@ -708,28 +703,6 @@ public class Database implements Closeable {
             return text;
         }
         logger.warn("Received unusable DeviceSqlString, returning empty string; lengthAndKind: " + string.lengthAndKind());
-        return "";
-    }
-
-    /**
-     * Helper function to extract the text value from one of the strings found in the database, which
-     * have a variety of obscure representations.
-     *
-     * @param string the string-encoding structure
-     *
-     * @return the text it contains, which may have zero length, but will never be {@code null}
-     */
-    public static String getText(RekordboxPdb.DeviceSqlLongString string) {
-        String text = null;
-        if (string.body() instanceof  RekordboxPdb.DeviceSqlLongAscii) {
-            text = ((RekordboxPdb.DeviceSqlLongAscii) string.body()).text();
-        } else if (string.body() instanceof  RekordboxPdb.DeviceSqlLongUtf16be) {
-            text = ((RekordboxPdb.DeviceSqlLongUtf16be) string.body()).text();
-        }
-        if (text != null) {
-            return text;
-        }
-        logger.warn("Received unusable DeviceSqlLongString, returning empty string; kind: " + string.kind());
         return "";
     }
 }
